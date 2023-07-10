07d8787c690381d1b63b56e547dc80e6ec0f124d
ShibAiko <115066823+ShibAiko@users.noreply.github.com>
2023-07-10 22:52:04 +0200

    Fonctionnalite Note OK

--- www/back/routes/addNotes.php
+++ www/back/routes/addNotes.php
@@ -1,27 +1,38 @@
 <?php
 session_start();
-include "connectBdd.php"; 
+include "connectBdd.php";
 
-$date = new \DateTime();
+if (!empty($_POST["addNotes"])) {
+    $date = new \DateTime();
+    $date_note = $date->format("Y-m-d H:i:s");
+    $contenu_note = $_POST["addNotes"];
+    $utilisateur_concerne = 1;
+    $auteur_note = $_SESSION['id_utilisateur'];
 
-$date_note = $date->format("Y-m-d H:i:s");
-$contenu_note = $_POST["addNotes"];
-$utilisateur_concerne = 1;
-$auteur_note = $_SESSION['id_utilisateur'];           
+    $sql = "INSERT INTO `notes` (`id_note`, `date_note`, `contenu_note`, `utilisateur_concerne`, `auteur_note`) VALUES (NULL, :date_note, :contenu_note, :utilisateur_concerne, :auteur_note) ";
+    $stmt = $bdd->prepare($sql);
+    $stmt->bindParam(":date_note", $date_note);
+    $stmt->bindParam(":contenu_note", $contenu_note);
+    $stmt->bindParam(":utilisateur_concerne", $utilisateur_concerne);
+    $stmt->bindParam(":auteur_note", $auteur_note);
 
-$sql = "INSERT INTO `notes` (`id_note`, `date_note`, `contenu_note`, `utilisateur_concerne`, `auteur_note`) VALUES (NULL, :date_note, :contenu_note, :utilisateur_concerne, :auteur_note) ";
-$stmt = $bdd->prepare($sql);
-$stmt->bindParam(":date_note", $date_note);
-$stmt->bindParam(":contenu_note", $contenu_note);
-$stmt->bindParam(":utilisateur_concerne", $utilisateur_concerne);
-$stmt->bindParam(":auteur_note", $auteur_note);
-
-if ($stmt->execute()) {
-    echo "<script type='text/javascript'>
-        alert('Note ajoutée avec succès!');
+    if ($stmt->execute()) {
+        echo "<script>
+            alert('Note ajoutée avec succès!');
+            window.location.href = '../../front/account.php';
+            </script>";
+        exit();
+    } else {
+        echo "<script type='text/javascript'>
+        alert('Erreur lors de l\'ajout de la note: " . $stmt->errorInfo()[2] . "');
+        window.history.back();
         </script>";
-        header('Location: ../../front/account.php');
+    exit();
+    }
 } else {
-    echo "Erreur lors de l'ajout de la note: " . $stmt->errorInfo()[2];
+    echo "<script>
+        alert('Le champ de texte est vide. La note n\'a pas été ajoutée.');
+        window.history.back();
+        </script>";
+    exit();
 }
-
--- www/front/assets/js/displayNotes.js
+++ www/front/assets/js/displayNotes.js
@@ -9,8 +9,11 @@
         .then(function (displayNotes){
             displayNotes.forEach(function (displayNote){
                 divNotes.innerHTML += `
-                    <p>${displayNote.date_note}<span class="whoWrote">${displayNote.nom_utilisateur} ${displayNote.prenom_utilisateur}</span>
-                    <a href="../back/routes/deleteNotes.php?id=${displayNote.id_note}"><span class="material-symbols-outlined delete"></span></a>
+                    <p>${displayNote.date_note}<span class="whoWrote">
+                        ${displayNote.nom_utilisateur}
+                        ${displayNote.prenom_utilisateur}</span>
+                        <a href="../back/routes/deleteNotes.php?id=${displayNote.id_note}">
+                        <span class="material-symbols-outlined delete"> delete </span></a>
                     </p>
                     <p class="textNote" >${displayNote.contenu_note}</p>
                     <hr>
